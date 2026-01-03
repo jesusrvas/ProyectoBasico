fbc9bc2b9e49b1b217dd0678663f67248546984a
jesusrvas <jesusrvas@gmail.com>
2026-01-03 00:03:51 -0400

    Estoy agregando script para consulta a la base de datos desdeuna URL de vercel

---
+++ datos.js
@@ -0,0 +1,12 @@
+async function cargarDatos() {
+try {
+// Reemplaza con la URL que te dé Render
+const response = await fetch('https://api-proyecto-basico-dek0ej8t9-jesus-r-s-projects.vercel.app');
+const lista = await response.json();
+console.log("Datos recibidos:", lista);
+// Aquí puedes usar DOM para mostrar los datos en tu HTML
+} catch (error) {
+console.error("Error al conectar:", error);
+}
+}
+cargarDatos();
